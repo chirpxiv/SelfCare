c8a4d7c1a804ec0d151701453af074fd9619f705
chirp <ch1rp@protonmail.com>
2023-01-17 17:23:24 +0000

    cleanup unused services

--- SelfCare/Services.cs
+++ SelfCare/Services.cs
@@ -1,25 +1,15 @@
-﻿using Dalamud.Plugin;
-using Dalamud.IoC;
+﻿using Dalamud.IoC;
+using Dalamud.Plugin;
 using Dalamud.Game;
-using Dalamud.Game.Gui;
 using Dalamud.Game.Command;
-using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Conditions;
-
-using FFXIVClientStructs.FFXIV.Client.Game.Control;
 
 namespace SelfCare {
 	internal class Services {
 		[PluginService] internal static DalamudPluginInterface Interface { get; set; } = null!;
 		[PluginService] internal static CommandManager CommandManager { get; set; } = null!;
-		[PluginService] internal static ObjectTable ObjectTable { get; set; } = null!;
-		[PluginService] internal static ClientState ClientState { get; set; } = null!;
 		[PluginService] internal static SigScanner SigScanner { get; set; } = null!;
 		[PluginService] internal static Condition Condition { get; set; } = null!;
-		[PluginService] internal static GameGui GameGui { get; set; } = null!;
-
-		internal unsafe static TargetSystem* Targets = TargetSystem.Instance();
 
 		internal static void Init(DalamudPluginInterface dalamud) => dalamud.Create<Services>();
 	}
